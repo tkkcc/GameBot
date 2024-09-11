fac53a046e9ba123bc046dbd48859aef09e768f6
bilabila <bilabila@qq.com>
2024-09-11 14:47:44 +0800

    simple and precise swipe for arknights

--- devtool/src/lib.rs
+++ devtool/src/lib.rs
@@ -6,6 +6,53 @@ use gamebot::{
 };
 use log::error;
 
+fn operator_swipe() {
+    let x0 = 1600.0;
+    touch_down(x0, 100.0, 0);
+    wait_millis(66);
+    touch_move(x0, 200.0, 0); // this need and affect x distance, it tells unity to scroll
+    wait_millis(200);
+    for i in 1..3 {
+        touch_move(x0 - 1080.0 * i as f32, 200.0, 0);
+        wait_millis(2000);
+    }
+    touch_move(x0, 200.0, 0);
+    wait_millis(1000);
+    touch_up(x0, 200.0, 0);
+}
+
+fn infrastructure_overview_swipe() {
+    let x0 = 1600.0;
+    touch_down(x0, 500.0, 0);
+    wait_millis(66);
+    touch_move(x0 + 1000.0, 500.0, 0); // this need and affect distance, it tells unity to scroll
+    wait_millis(200);
+    for i in 1..12 {
+        touch_move(x0, 500.0 - i as f32 * 500.0, 0);
+        wait_millis(1000);
+    }
+    touch_move(x0, 500.0, 0);
+    wait_millis(1000);
+    touch_up(x0, 500.0, 0);
+}
+
+fn fight_swipe() {
+    // swipe to first fight
+    touch_down(1000.0, 300.0, 0);
+    wait_millis(66); // must
+    touch_move(10000.0, 300.0, 0);
+    wait_millis(66); // must
+    touch_up(10000.0, 300.0, 0);
+
+    // nav
+    touch_down(1000.0, 300.0, 0);
+    wait_millis(66);
+    touch_move(-4000.0, 300.0, 0);
+    wait_millis(66);
+    touch_move(-4000.0, 301.0, 0);
+    wait_millis(66);
+    touch_up(-4000.0, 301.0, 0);
+}
 fn start() {
     error!("what");
 
@@ -13,72 +60,7 @@ fn start() {
     wait_millis(500);
     click_recent();
     wait_secs(1);
-    let start_time = Instant::now();
-
-    touch_down(1000.0, 300.0, 0);
-    wait_millis(200);
-    touch_down(1000.0, 300.0, 1);
-    wait_millis(200);
-    // touch_move(0.0, 300.0, 0);
-    // touch_move(2000.0, 300.0, 1);
-    // let x2 = 500.0;
-    // wait_millis(2000);
-    // for i in (0..1000) {
-    //     wait_millis(33);
-    //     touch_move(1000.0 - i as f32 * 5.0, 300.0);
-    // }
-    // wait_millis(200);
-    // touch_move(x2, 301.0);
-    wait_millis(2000);
-    touch_up(0.0, 300.0, 0);
-    wait_millis(200);
-    touch_up(2000.0, 300.0, 1);
-
-    /// fight nav
-    //
-    // touch_down(1000.0, 300.0);
-    // wait_millis(66); // must
-    // touch_move(10000.0, 300.0);
-    // wait_millis(66); // must
-    // touch_up(10000.0, 300.0);
-    //
-    // touch_down(1000.0, 300.0);
-    // wait_millis(66);
-    // touch_move(-4000.0, 300.0);
-    // wait_millis(66);
-    // touch_move(-4000.0, 301.0);
-    // wait_millis(66);
-    // touch_up(-4000.0, 301.0);
-    for i in (0..) {
-        // let start_time = Instant::now();
-        // click(400, 400);
-        // log!(start_time.elapsed());
-        // toast2("what1");
-        // let screenshot = take_screenshot();
-        // ssleep(1);
-        break;
-
-        // log!(screenshot);
-        // break;
-
-        // error!(screenshot);
-        // error!(
-        //     "{}x{} {}",
-        //     screenshot.width,
-        //     screenshot.height,
-        //     screenshot.data.len()
-        // );
-    }
-
-    error!("10000x {:?}", start_time.elapsed());
-
-    thread::spawn(|| {
-        toast("what2");
-        wait_secs(1);
-    });
-
-    wait_secs(3);
-    toast("what3");
+    infrastructure_overview_swipe();
 }
 
 gamebot::entry!(start);
